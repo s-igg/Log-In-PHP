22e45e4555aabc0893fe357aa3d43735f917f04d
Sigge <sergejvovk99@gmail.com>
2018-01-16 12:29:39 +0200

    Added LogOut function and more

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title><?php echo $title; ?> | App</title>
-    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/style.css">
   </head>
 <body>
--- index2.php
+++ index2.php
@@ -2,10 +2,15 @@
 $title = "Welcome";
   include 'includes/header.php';
   session_start();
-  if (!$_SESSION['LogIn']) {
+/*  if (!$_SESSION['LogIn']) {
     header("Location: login.php");
-  }
+  }*/
 ?>
-<h1>┬┴┬┴┤ ͜ʖ ͡°) ├┬┴┬┴ <?php echo $_SESSION['Username']; ?></h1>
+<?php if ($_SESSION['Username']) : ?>
+  <h1>┬┴┬┴┤ ͜ʖ ͡°) ├┬┴┬┴ <?php echo $_SESSION['Username']; ?></h1>
+  <a href="LogOut.php">Log Out</a>
+<?php else : ?>
+  <h1>Hi!</h1>
+<?php endif; ?>
 </body>
 </html>
