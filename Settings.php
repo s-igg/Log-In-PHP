1a9b946b35e888ee1ddde208eb7b4b5950a72528
Sigge <sergejvovk99@gmail.com>
2018-02-15 14:01:51 +0200

    ...

--- Settings.php
+++ Settings.php
@@ -3,23 +3,29 @@
   session_start();
   include 'includes/header.php';
   include 'includes/nav.php';
+
+  if (isset($_POST['saveProfilePic'])) {
+    $profilepic = $_FILES['ProfilePic']['name'];
+    $profilepic_temp = $_FILES['ProfilePic']['tmp_name'];
+    move_uploaded_file($profilepic_temp, "img/profile-img/$profilepic");
+  }
 ?>
 
 
 <div class="container-fluid">
   <div class="row justify-content-center align-items-center">
-    <form class="col-12 col-sm-8 col-lg-4 usersForm UglySolutFixlater" action="Settings.php" method="post">
-    <div class="form-group">
-      <input type="text" name="username" class="form-control" placeholder="Username" autofocus>
-    </div>
-    <button type="submit" name="login" class="col-12  btn btn-primary">Save</button>
-      <?php if(isset($taken)): ?>
-        <div class="alert alert-danger animated flash"><?php echo $taken; ?></div>
-      <?php endif; ?>
-    </form>
-  </div>
-</div>
 
+    <form class="col-12 col-sm-8 col-lg-4 usersForm UglySolutFixlater" action="Settings.php" method="post" enctype="multipart/form-data">
+      <div class="custom-file">
+          <input type="file" name="ProfilePic" class="custom-file-input" id="validatedCustomFile" required>
+          <label class="custom-file-label" for="validatedCustomFile">Choose file...</label>
+          <div class="invalid-feedback">Somthing went wrong</div>
+        </div>
+        <button type="submit" name="saveProfilePic" class="col-12 btn btn-primary">Save</button>
+    </form>
+
+  </div>
+  </div>
 
 
 <?php include 'includes/footer.php'; ?>
